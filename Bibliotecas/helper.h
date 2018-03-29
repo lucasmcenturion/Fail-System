def507f63797d4c7c9c436044607242cf131ef01
lucasmcenturion <lucasmcenturion@gmail.com>
2018-03-29 14:42:36 -0300

    Subo bibliotecas que nos van a ser útiles este cuatrimestre

---
+++ Bibliotecas/helper.h
@@ -0,0 +1,199 @@
+#ifndef HELPER_
+#define HELPER_
+#define LAMBDA(c_) ({ c_ _;}) //Para funciones lambda
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+#include <commons/temporal.h>
+#include <commons/log.h>
+#include <commons/config.h>
+#include <commons/collections/list.h>
+#include <commons/collections/dictionary.h>
+#include <commons/collections/queue.h>
+#include <commons/string.h>
+#include <commons/txt.h>
+#include <commons/bitarray.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <time.h>
+#include <string.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <limits.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <dirent.h>
+#include <ctype.h>
+#include <semaphore.h>
+#include <assert.h>
+#include <errno.h>
+#include <signal.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdbool.h>
+
+#define true 1
+#define false 0
+#define NULL ((void *) 0)
+
+typedef enum { TRANSFORMACION, REDUCCIONLOCAL, REDUCCIONGLOBAL, ALMACENAMIENTOFINAL} etapa;
+typedef enum { ENPROCESO, ERROR, FINALIZADOOK} estado;
+typedef enum { ALMACENARARCHIVO, LEERARCHIVO} interfazFilesystem;
+
+typedef struct{
+	char* nodo;
+	char* ip;
+	uint32_t puerto;
+	uint32_t cargaDeTrabajo;
+	uint32_t disponibilidad;
+	uint32_t contTareasRealizadas;
+	uint32_t indice;
+}
+__attribute__((packed))
+datosWorker;
+
+typedef struct{
+	int id;
+	int socket;
+	int contJobs;
+}
+__attribute__((packed))
+master;
+
+typedef struct {
+	datosWorker worker;
+	int nodo;
+	int bloque;
+	int bytesOcupados;
+	char* archTemp;
+}__attribute__((packed))
+transformacionDatos;
+
+
+
+typedef struct {
+	int numero_bloque;
+	int tamanio;
+	int primer_bloque_nodo;
+	int segundo_bloque_nodo;
+	char*primer_nombre_nodo;
+	char*segundo_nombre_nodo;
+}__attribute__((packed))t_bloque_yama;
+
+typedef struct {
+	bool resultado;
+	int bloque;
+	char*nombre_nodo;
+}__attribute__((packed))
+t_resultado_envio;
+
+typedef struct {
+	int bloque;
+	char*nombre_nodo;
+}__attribute__((packed))
+t_bloque_enviado;
+
+typedef struct {
+	datosWorker worker;
+	char** listaArchivosTemps; //Lista de nombres de archivos temporales de cada worker
+	char* archTemp;
+}__attribute__((packed))
+reduccionLocalDatos;
+
+typedef struct {
+	datosWorker worker;
+	datosWorker WorkerEncargado;
+	int nodoEncargado;
+	char** listaArchivosTemps; //Lista de nombres de archivos temporales de cada worker
+	char* archTemp;
+}__attribute__((packed))
+reduccionGlobalDatos;
+
+typedef struct {
+	datosWorker worker;
+	char* progTrans; //Programa de Transformacion
+	int bloques;
+	int bytesOcupados;
+	char* archTemp;
+}__attribute__((packed))
+datosTransWorker;
+
+typedef struct {
+	int job;
+	int master;
+	char* nodo;
+	char* nodoConOtraCopia;
+	int nroBloque;
+	int nroBloqueCopia;
+	int bloque;
+	etapa etapa;
+	char* archivoTemporal;
+	char* archivoFinal;
+	estado estado;
+	int tamanio;
+}__attribute__((packed))
+registroEstado;
+
+typedef struct{
+	uint32_t socket;
+	uint32_t bloques_totales;
+	uint32_t bloques_libres;
+	uint32_t puertoworker;
+	char *nodo;
+	char *ip_nodo;
+}__attribute__((packed))
+info_datanode;
+
+typedef struct{
+	char*nombre_nodo;
+	int cantidad;
+}__attribute__((packed))
+t_solicitudes;
+
+typedef struct{
+ int numero;
+ int copia;
+ int tamanio;
+ void *datos;
+ char *nombre_archivo;
+}__attribute__((packed)) bloque;
+
+typedef struct{
+ char *nombre;
+ uint32_t cantidad;
+ uint32_t socket;
+}__attribute__((packed)) t_datanode_a_enviar;
+
+typedef struct{
+ int index;
+ char nombre[255];
+ int padre;
+}__attribute__((packed)) t_directory;
+
+typedef struct{
+ int index;
+ int socket;
+ int cantidad;
+ char* nombre;
+}__attribute__((packed)) t_dtdisponible;
+typedef struct{
+ int tamanio;
+ char*nombre_nodo;
+ void*datos;
+}__attribute__((packed)) t_cpblock;
+
+typedef struct{
+ int index_padre;
+ int total_bloques;
+ int tipo;
+ char *nombre_archivo;
+ pthread_mutex_t mutex;
+}__attribute__((packed)) t_archivo_actual;
+
+void escribir_log(char* nombre_log,char* proceso,char* mensaje,char* tipo);
+char* integer_to_string(char*string,int x);
+size_t getFileSize(const char* filename);
+
+#endif /* HELPER_*/
---
+++ Bibliotecas/sockets.h
@@ -0,0 +1,149 @@
+#ifndef SOCKETS_H_
+#define SOCKETS_H_
+
+#include "helper.h"
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/sendfile.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#define BACKLOG 10 // Cuántas conexiones pendientes se mantienen en cola del server
+#define TAMANIOHEADER sizeof(Header)
+#define STRHANDSHAKE "10"
+//Emisores:
+#define MASTER "Master    "
+#define WORKER "Worker    "
+#define FILESYSTEM "FileSystem"
+#define DATANODE "Datanode  "
+#define YAMA "YAMA      "
+
+typedef enum { ESHANDSHAKE, ESDATOS, ESSTRING, ESARCHIVO, ESINT, ESERROR, NUEVOWORKER, TRANSFWORKER, VALIDACIONWORKER,
+	REDLOCALWORKER, REDGLOBALWORKER, VALIDACIONYAMA,ESNOMBRE,IDENTIFICACIONDATANODE,ESBLOQUE,GETBLOQUE,SETBLOQUE,
+	RESULOPERACION,BLOQUEOBTENIDO, SOLICITUDBLOQUE , RESPUESTASOLICITUD, NODODESCONECTADO, SOLICITUDBLOQUESYAMA,
+	SOLICITUDTRANSFORMACION, SOLICITUDREDUCCIONLOCAL, SOLICITUDREDUCCIONGLOBAL, SOLICITUDALMACENADOFINAL,SOLICITUDARCHTEMP, ARCHIVOTEMPRL,FINARCHIVOTEMPRL,LISTAWORKERS,
+	FINTRANSFORMACION, FINREDUCCIONLOCAL, FINREDUCCIONGLOBAL, FINALMACENADOFINAL, SOLICITUDBLOQUECPTO,RESPUESTASOLICITUDCPTO,
+	RESPUESTAGETBLOQUE,SETBLOQUECPTO,RESPUESTASETBLOQUECPTO} tipo;
+
+typedef enum { FORMATEAR, REMOVER, RENOMBRAR, MOVER, MOSTRAR, CREARDIR, CPFROM, CPTO, CPBLOCK, MD5, LISTAR, INFO} accionesFilesystem;
+
+typedef struct{
+	etapa header;
+	datosWorker worker;
+	int bloque;
+	char* archivoTemporal;
+	char* programa;
+	int cantidadDeBytesOcupados;
+	char** ListaArchivosTemporales;
+	int cantArchTempRL;
+	datosWorker workerEncargado;
+}__attribute__((packed)) solicitudPrograma;
+
+typedef struct{
+	int bloque;
+	int idJob;//jXnYbZesX;
+	char* nodo;
+	bool exito;
+}__attribute__((packed)) rtaEstado;
+
+typedef struct{
+	char* rutaArchivoF; //Esta es la ruta para FS
+	char* resultRG; //Esta es la ruta de archivoTemporal
+	datosWorker worker;
+}__attribute__((packed)) datoAF; //esto es para Master y Worker
+
+typedef struct{
+	char* buffer;
+	char* ruta;
+}__attribute__((packed)) nodoAF; //Esto es para FS
+
+typedef struct{
+	etapa header; //No se si es necesaria, ahora lo chequeo.
+	datosWorker worker;
+	char* archTempRL;
+	bool encargado;
+}__attribute__((packed)) nodoRG;
+
+typedef struct{
+	etapa header;
+	t_list* nodos;
+	char* archRG; //Archivo destino
+	char* programaR;
+	int cantNodos;
+	int idJob;
+}__attribute__((packed)) solicitudRG;
+
+typedef struct{
+	etapa header;
+	datosWorker worker;
+	int bloque;
+	int bytesOcupados;
+	char* archivoTemporal;
+	char* programaT;
+}__attribute__((packed)) nodoT; //Recibo/Envias una lista de nodos (o array)
+
+typedef struct{
+	etapa header;
+	datosWorker worker;
+	t_list* listaArchivosTemporales;
+	char* archivoTemporal; //Destino
+	char* programaR;
+	int idJob;
+}__attribute__((packed)) nodoRL; //Recibo/Envias una lista de nodos (o array)
+
+
+typedef struct {
+	tipo tipoMensaje;
+	uint32_t tamPayload;
+	char emisor[11];
+}__attribute__((packed)) Header;
+
+
+typedef struct {
+	int bloques_totales;
+	int bloque_nodo;
+	int bloque_archivo;
+	int tamanio_bloque;
+	int index_directorio;
+	char* nombre_archivo;
+	char* nombre_nodo;
+}__attribute__((packed)) t_solicitud_bloque;
+
+typedef struct {
+	Header header;
+	void* Payload;
+}__attribute__((packed)) Paquete;
+
+typedef struct {
+	pthread_t hilo;
+	int socket;
+} structHilo;
+
+typedef struct {
+	pid_t pid;
+	int socket;
+} structProceso;
+
+void Servidor(char* ip, int puerto, char nombre[11],
+		void (*accion)(Paquete* paquete, int socketFD),
+		int (*RecibirPaquete)(int socketFD, char receptor[11], Paquete* paquete));
+void ServidorConcurrente(char* ip, int puerto, char nombre[11], t_list** listaDeHilos,
+		bool* terminar, void (*accionHilo)(void* socketFD));
+void ServidorConcurrenteForks(char* ip, int puerto, char nombre[11], t_list** listaDeProcesos,
+		bool* terminar, void (*accionPadre)(void* socketFD), void (*accionHijo)(void* socketFD));
+int StartServidor(char* MyIP, int MyPort);
+int ConectarAServidor(int puertoAConectar, char* ipAConectar, char servidor[11], char cliente[11],
+		void RecibirElHandshake(int socketFD, char emisor[11])); //Sobrecarga para Kernel
+
+bool EnviarDatos(int socketFD, char emisor[11], void* datos, int tamDatos);
+bool EnviarDatosTipo(int socketFD, char emisor[11], void* datos, int tamDatos, tipo tipoMensaje);
+bool EnviarMensaje(int socketFD, char* msg, char emisor[11]);
+bool EnviarPaquete(int socketCliente, Paquete* paquete);
+void RecibirHandshake(int socketFD, char emisor[11]);
+int RecibirPaqueteServidor(int socketFD, char receptor[11], Paquete* paquete); //Responde al recibir un Handshake
+int RecibirPaqueteCliente(int socketFD, char receptor[11], Paquete* paquete); //No responde los Handshakes
+
+#endif //SOCKETS_H_
