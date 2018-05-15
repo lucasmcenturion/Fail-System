a0a8c7381259016874fd3c18f5d7334ea7ca1463
Nadia Gutierrez <nadiagutierrez89@yahoo.com.ar>
2018-05-15 14:02:30 -0300

    Subo validaciones para archivo de configuración y logueo

--- Coordinador/src/Coordinador.c
+++ Coordinador/src/Coordinador.c
@@ -3,45 +3,66 @@
 
 /* Variables Globales */
 char *IP, *ALGORITMO_DISTRIBUCION;
-int PUERTO, CANT_ENTRADAS, TAMANIO_ENTRADA, RETARDO,socketPlanificador;
+int PUERTO, CANT_ENTRADAS, TAMANIO_ENTRADA, RETARDO, socketPlanificador;
 t_list* listaHilos;
 bool end;
-t_log * vg_logger = NULL;
 t_list* instancias;
 t_list* esis;
 t_list* claves_globales;
 pthread_mutex_t mutex_instancias;
 pthread_mutex_t mutex_esis;
 pthread_mutex_t mutex_claves;
+t_log* vg_logger;
 
-void obtenerValoresArchivoConfiguracion() {
-	t_config* arch = config_create("/home/utnso/workspace/tp-2018-1c-Fail-system/Coordinador/coordinador.cfg");
-	IP = string_duplicate(config_get_string_value(arch, "IP"));
-	PUERTO = config_get_int_value(arch, "PUERTO");
-	ALGORITMO_DISTRIBUCION = string_duplicate(config_get_string_value(arch, "ALGORITMO_DISTRIBUCION"));
+/*Creación de Logger*/
+void crearLogger() {
+	vg_logger = log_create("CoordinadorLog.log", "Coordinador", true,
+			LOG_LEVEL_INFO);
+}
+//destroy logger
+void finalizarLogger() {
+	log_destroy(vg_logger);
+
+}
+
+void obteneryValidarValoresArchivoConfiguracion() {
+	log_info(vg_logger, "Chequeo archivo de configuración");
+	t_config* arch =
+			config_create(
+					"/home/utnso/workspace/tp-2018-1c-Fail-system/Coordinador/coordinador.cfg");
+	if (config_has_property(arch, "IP") == true
+			&& strlen(config_get_string_value(arch, "IP")) > 7) {
+		IP = string_duplicate(config_get_string_value(arch, "IP"));
+		log_info(vg_logger, "IP: %s", IP);
+	} else {
+		logearError("IP erroneo en Coordinador.cfg");
+	}
+	if (config_has_property(arch, "PUERTO") == true
+			&& config_get_string_value(arch, "PUERTO") > 1) {
+		PUERTO = config_get_int_value(arch, "PUERTO");
+		log_info(vg_logger, "PUERTO: %d", PUERTO);
+	} else {
+		logearError("PUERTO erroneo en Coordinador.cfg");
+	}
+	ALGORITMO_DISTRIBUCION = string_duplicate(
+			config_get_string_value(arch, "ALGORITMO_DISTRIBUCION"));
 	CANT_ENTRADAS = config_get_int_value(arch, "CANT_ENTRADAS");
 	TAMANIO_ENTRADA = config_get_int_value(arch, "TAMANIO_ENTRADA");
 	RETARDO = config_get_int_value(arch, "RETARDO");
 	config_destroy(arch);
+
 }
 
-void imprimirArchivoConfiguracion(){
+void imprimirArchivoConfiguracion() {
 
-	printf(
-				"Configuración:\n"
-				"IP=%s\n"
-				"PUERTO=%d\n"
-				"ALGORITMO_DISTRIBUCION=%s\n"
-				"CANT_ENTRADAS=%d\n"
-				"TAMANIO_ENTRADA=%d\n"
-				"RETARDO=%d\n",
-				IP,
-				PUERTO,
-				ALGORITMO_DISTRIBUCION,
-				CANT_ENTRADAS,
-				TAMANIO_ENTRADA,
-				RETARDO
-				);
+	printf("Configuración:\n"
+			"IP=%s\n"
+			"PUERTO=%d\n"
+			"ALGORITMO_DISTRIBUCION=%s\n"
+			"CANT_ENTRADAS=%d\n"
+			"TAMANIO_ENTRADA=%d\n"
+			"RETARDO=%d\n", IP, PUERTO, ALGORITMO_DISTRIBUCION, CANT_ENTRADAS,
+			TAMANIO_ENTRADA, RETARDO);
 	fflush(stdout);
 }
 
@@ -49,37 +70,40 @@ int comprobarValoresBienSeteados() {
 
 	int retorno = 1;
 
-	if (!PUERTO) {
+	if (PUERTO != 8000) {
 		logearError(
 				"Archivo config de Coordinador: el puerto del COORDINADOR está mal configurado.");
 		retorno = 0;
 	}
-
-
+	log_info(vg_logger, "Puerto Correcto");
 	return retorno;
 }
-int getProximo(){
-	if(list_size(instancias)==0)
+int getProximo() {
+	if (list_size(instancias) == 0)
 		return 0;
 	t_IdInstancia *aux;
 
-	if(list_all_satisfy(instancias,LAMBDA(bool _(t_IdInstancia * elemento) { return elemento->activo==true;}))){
-		list_iterate(instancias, LAMBDA(void _(t_IdInstancia * elemento) {  elemento->activo=false;}));
-		aux= list_get(instancias,0);
-		aux->activo=true;
-		list_replace(instancias,0,aux);
+	if (list_all_satisfy(instancias,
+			LAMBDA(
+					bool _(t_IdInstancia * elemento) { return elemento->activo==true;}))) {
+		list_iterate(instancias,
+				LAMBDA(
+						void _(t_IdInstancia * elemento) { elemento->activo=false;}));
+		aux = list_get(instancias, 0);
+		aux->activo = true;
+		list_replace(instancias, 0, aux);
 		return aux->socket;
 	}
-	int i=-1;
-	bool proximo (t_IdInstancia *elemento){
+	int i = -1;
+	bool proximo(t_IdInstancia *elemento) {
 		i++;
-		if(!elemento->activo)
+		if (!elemento->activo)
 			return true;
 	}
 	list_find(instancias, proximo);
-	aux= list_get(instancias, i);
-	aux->activo=true;
-	list_replace(instancias,i,aux);
+	aux = list_get(instancias, i);
+	aux->activo = true;
+	list_replace(instancias, i, aux);
 	return aux->socket;
 }
 
@@ -91,153 +115,176 @@ void sacar_instancia(int socket) {
 	instancias = list_filter(instancias, (void*) tiene_socket);
 }
 
-
 void accion(void* socket) {
 	int socketFD = *(int*) socket;
 	Paquete paquete;
 	void* datos;
-	while (RecibirPaqueteServidorCoordinador(socketFD, COORDINADOR, &paquete) > 0) {
-	//SWITCH
+	while (RecibirPaqueteServidorCoordinador(socketFD, COORDINADOR, &paquete)
+			> 0) {
+		//SWITCH
 		if (!strcmp(paquete.header.emisor, INSTANCIA)) {
-			switch(paquete.header.tipoMensaje){
-				case ESHANDSHAKE:{
-					EnviarDatosTipo(socketFD,COORDINADOR,1,0,SOLICITUDNOMBRE);
-					int tamanioDatosEntradas=sizeof(int)*2;
-					void *datosEntradas=malloc(tamanioDatosEntradas);
-					*((int*)datosEntradas)=TAMANIO_ENTRADA;
-					datosEntradas+=sizeof(int);
-					*((int*)datosEntradas)=CANT_ENTRADAS;
-					datosEntradas+=sizeof(int);
-					datosEntradas-=tamanioDatosEntradas;
-					EnviarDatosTipo(socketFD,COORDINADOR,datosEntradas,tamanioDatosEntradas,GETENTRADAS);
-					free(datosEntradas);
-				}
+			switch (paquete.header.tipoMensaje) {
+			case ESHANDSHAKE: {
+				EnviarDatosTipo(socketFD, COORDINADOR, 1, 0, SOLICITUDNOMBRE);
+				int tamanioDatosEntradas = sizeof(int) * 2;
+				void *datosEntradas = malloc(tamanioDatosEntradas);
+				*((int*) datosEntradas) = TAMANIO_ENTRADA;
+				datosEntradas += sizeof(int);
+				*((int*) datosEntradas) = CANT_ENTRADAS;
+				datosEntradas += sizeof(int);
+				datosEntradas -= tamanioDatosEntradas;
+				EnviarDatosTipo(socketFD, COORDINADOR, datosEntradas,
+						tamanioDatosEntradas, GETENTRADAS);
+				free(datosEntradas);
+			}
 				break;
-				case IDENTIFICACIONINSTANCIA: {
-					char *nombreInstancia = malloc(paquete.header.tamPayload);
-					strcpy(nombreInstancia,(char*)paquete.Payload);
-					t_IdInstancia *instancia= malloc(sizeof(t_IdInstancia));
-					instancia->socket=socketFD;
-					instancia->nombre=malloc(strlen(nombreInstancia)+1);
-					instancia->activo = false;
-					instancia->claves = list_create();
-					strcpy(instancia->nombre,nombreInstancia);
-					pthread_mutex_lock(&mutex_instancias);
-					list_add(instancias,instancia);
-					pthread_mutex_unlock(&mutex_instancias);
-				}
+			case IDENTIFICACIONINSTANCIA: {
+				char *nombreInstancia = malloc(paquete.header.tamPayload);
+				strcpy(nombreInstancia, (char*) paquete.Payload);
+				t_IdInstancia *instancia = malloc(sizeof(t_IdInstancia));
+				instancia->socket = socketFD;
+				instancia->nombre = malloc(strlen(nombreInstancia) + 1);
+				instancia->activo = false;
+				instancia->claves = list_create();
+				strcpy(instancia->nombre, nombreInstancia);
+				pthread_mutex_lock(&mutex_instancias);
+				list_add(instancias, instancia);
+				pthread_mutex_unlock(&mutex_instancias);
+			}
 				break;
-				case SETOK:
-				{
-					int tiene_socket(t_IdInstancia *e) {
-						if (e->socket == socketFD)
-							return e->socket != socketFD;
-					}
-					pthread_mutex_lock(&mutex_instancias);
-					list_add(((t_IdInstancia*)list_find(instancias, tiene_socket))->claves,(char*)paquete.Payload);
-					pthread_mutex_unlock(&mutex_instancias);
+			case SETOK: {
+				int tiene_socket(t_IdInstancia *e) {
+					if (e->socket == socketFD)
+						return e->socket != socketFD;
 				}
+				pthread_mutex_lock(&mutex_instancias);
+				list_add(
+						((t_IdInstancia*) list_find(instancias, tiene_socket))->claves,
+						(char*) paquete.Payload);
+				pthread_mutex_unlock(&mutex_instancias);
+			}
 				break;
 			}
 		}
-		if(!strcmp(paquete.header.emisor, ESI)){
-			switch(paquete.header.tipoMensaje){
-				case ESHANDSHAKE:{
-					t_esiCoordinador*nuevo =malloc(sizeof(t_esiCoordinador));
-					nuevo->id=malloc(paquete.header.tamPayload);
-					strcpy(nuevo->id,paquete.Payload);
-					nuevo->socket = socketFD;
-					pthread_mutex_lock(&mutex_esis);
-					list_add(esis,nuevo);
-					pthread_mutex_unlock(&mutex_esis);
-				}
+		if (!strcmp(paquete.header.emisor, ESI)) {
+			switch (paquete.header.tipoMensaje) {
+			case ESHANDSHAKE: {
+				t_esiCoordinador*nuevo = malloc(sizeof(t_esiCoordinador));
+				nuevo->id = malloc(paquete.header.tamPayload);
+				strcpy(nuevo->id, paquete.Payload);
+				nuevo->socket = socketFD;
+				pthread_mutex_lock(&mutex_esis);
+				list_add(esis, nuevo);
+				pthread_mutex_unlock(&mutex_esis);
+			}
 				break;
-				case SETCOORD:{
-					usleep(RETARDO);
-					char*key=malloc(strlen(datos)+1);
-					strcpy(key,datos);
-					datos+=strlen(datos)+1;
-					char* value=malloc(strlen(datos)+1);
-					strcpy(value,datos);
+			case SETCOORD: {
+				usleep(RETARDO);
+				char*key = malloc(strlen(datos) + 1);
+				strcpy(key, datos);
+				datos += strlen(datos) + 1;
+				char* value = malloc(strlen(datos) + 1);
+				strcpy(value, datos);
 
-					char*id=malloc(10);
-					strcpy(id,((t_esiCoordinador*)list_find(esis,LAMBDA(int _(t_esiCoordinador *elemento) {  return elemento->socket ==socketFD;})))->id);
-					id=realloc(id,strlen(id)+1);
-					bool foo(char*e){
-						return !strcmp(e,key);
-					}
-					bool verificarClave(t_IdInstancia *e){
-						return list_any_satisfy(e->claves,LAMBDA(int _(char *clave) {  return !strcmp(clave,key);}));
-					}
-					pthread_mutex_lock(&mutex_claves);
-					if(list_any_satisfy(claves_globales, foo)){
-						if(!list_any_satisfy(instancias, verificarClave)){
-							//clave eviste en el sistema, pero la instancia esta caida
-							printf("Se intenta bloquear la clave %s pero en este momento no esta disponible",key);
-							fflush(stdout);
-							EnviarDatosTipo(socketPlanificador,COORDINADOR,id,strlen(id)+1,ABORTAR);
-						}else{
-							int tam=strlen(key)+strlen(value)+2;
-							void*sendInstancia = malloc(tam);
-							strcpy(sendInstancia,key);
-							sendInstancia+=strlen(key)+1;
-							strcpy(sendInstancia,value);
-							sendInstancia-=strlen(value)+1;
-							sendInstancia-=tam;
-							if(!strcmp(ALGORITMO_DISTRIBUCION,"EL")){
-								pthread_mutex_lock(&mutex_instancias);
-								int socketSiguiente = getProximo();
-								if(socketSiguiente!=0){
-									printf("%s\n",socketSiguiente);
-									fflush(stdout);
-									EnviarDatosTipo(socketSiguiente,COORDINADOR,sendInstancia,tam,SETINST);
-								}else{
-									//error, no hay instancias conectadas al sistema
+				char*id = malloc(10);
+				strcpy(id,
+						((t_esiCoordinador*) list_find(esis,
+								LAMBDA(int _(t_esiCoordinador *elemento) {
+									return elemento->socket == socketFD;
 								}
-								pthread_mutex_unlock(&mutex_instancias);
+						)))->id);
+				id = realloc(id, strlen(id) + 1);
+				bool foo(char*e) {
+					return !strcmp(e, key);
+				}
+				bool verificarClave(t_IdInstancia *e) {
+					return list_any_satisfy(e->claves,
+							LAMBDA(int _(char *clave) {
+								return !strcmp(clave, key);
 							}
-							free(sendInstancia);
+					));
+				}
+				pthread_mutex_lock(&mutex_claves);
+				if (list_any_satisfy(claves_globales, foo)) {
+					if (!list_any_satisfy(instancias, verificarClave)) {
+						//clave eviste en el sistema, pero la instancia esta caida
+						printf(
+								"Se intenta bloquear la clave %s pero en este momento no esta disponible",
+								key);
+						fflush(stdout);
+						EnviarDatosTipo(socketPlanificador, COORDINADOR, id,
+								strlen(id) + 1, ABORTAR);
+					} else {
+						int tam = strlen(key) + strlen(value) + 2;
+						void*sendInstancia = malloc(tam);
+						strcpy(sendInstancia, key);
+						sendInstancia += strlen(key) + 1;
+						strcpy(sendInstancia, value);
+						sendInstancia -= strlen(value) + 1;
+						sendInstancia -= tam;
+						if (!strcmp(ALGORITMO_DISTRIBUCION, "EL")) {
+							pthread_mutex_lock(&mutex_instancias);
+							int socketSiguiente = getProximo();
+							if (socketSiguiente != 0) {
+								printf("%s\n", socketSiguiente);
+								fflush(stdout);
+								EnviarDatosTipo(socketSiguiente,
+								COORDINADOR, sendInstancia, tam, SETINST);
+							} else {
+								//error, no hay instancias conectadas al sistema
+							}
+							pthread_mutex_unlock(&mutex_instancias);
 						}
-					}else{
-						//clave no existe en el sistema
-						printf("Se intenta bloquear la clave %s pero no existe",key);
-						EnviarDatosTipo(socketPlanificador,COORDINADOR,id,strlen(id)+1,ABORTAR);
+						free(sendInstancia);
 					}
-					pthread_mutex_unlock(&mutex_claves);
-					free(key);
-					free(value);
-					free(id);
+				} else {
+					//clave no existe en el sistema
+					printf("Se intenta bloquear la clave %s pero no existe",
+							key);
+					EnviarDatosTipo(socketPlanificador, COORDINADOR, id,
+							strlen(id) + 1, ABORTAR);
 				}
+				pthread_mutex_unlock(&mutex_claves);
+				free(key);
+				free(value);
+				free(id);
+			}
 				break;
-				case GETCOORD: {
-					usleep(RETARDO);
-					pthread_mutex_lock(&mutex_claves);
-					list_add(claves_globales,(char*)paquete.Payload);
-					pthread_mutex_unlock(&mutex_claves);
-					char*id=malloc(10);
-					strcpy(id,((t_esiCoordinador*)list_find(esis,LAMBDA(int _(t_esiCoordinador *elemento) {  return elemento->socket ==socketFD;})))->id);
-					id=realloc(id,strlen(id)+1);
-					int tamSend=strlen(paquete.Payload)+strlen(id)+2;
-					void* sendPlanificador = malloc(tamSend);
-					strcpy(sendPlanificador,paquete.Payload);
-					sendPlanificador+=strlen(paquete.Payload)+1;
-					strcpy(sendPlanificador,id);
-					sendPlanificador+=strlen(id)+1;
-					sendPlanificador-=tamSend;
-					EnviarDatosTipo(socketPlanificador,COORDINADOR,sendPlanificador,tamSend,GETPLANI);
-				}
+			case GETCOORD: {
+				usleep(RETARDO);
+				pthread_mutex_lock(&mutex_claves);
+				list_add(claves_globales, (char*) paquete.Payload);
+				pthread_mutex_unlock(&mutex_claves);
+				char*id = malloc(10);
+				strcpy(id,
+						((t_esiCoordinador*) list_find(esis,
+								LAMBDA(int _(t_esiCoordinador *elemento) {
+									return elemento->socket == socketFD;
+								}
+						)))->id);
+				id = realloc(id, strlen(id) + 1);
+				int tamSend = strlen(paquete.Payload) + strlen(id) + 2;
+				void* sendPlanificador = malloc(tamSend);
+				strcpy(sendPlanificador, paquete.Payload);
+				sendPlanificador += strlen(paquete.Payload) + 1;
+				strcpy(sendPlanificador, id);
+				sendPlanificador += strlen(id) + 1;
+				sendPlanificador -= tamSend;
+				EnviarDatosTipo(socketPlanificador, COORDINADOR,
+						sendPlanificador, tamSend, GETPLANI);
+			}
 				break;
-				case STORECOORD:{
-					usleep(RETARDO);
-				}
+			case STORECOORD: {
+				usleep(RETARDO);
+			}
 				break;
 			}
 		}
-		if(!strcmp(paquete.header.emisor, PLANIFICADOR)){
-			switch(paquete.header.tipoMensaje){
-				case ESHANDSHAKE: {
-					socketPlanificador = socketFD;
-				}
+		if (!strcmp(paquete.header.emisor, PLANIFICADOR)) {
+			switch (paquete.header.tipoMensaje) {
+			case ESHANDSHAKE: {
+				socketPlanificador = socketFD;
+			}
 				break;
 			}
 		}
@@ -251,16 +298,17 @@ void accion(void* socket) {
 }
 
 int main(void) {
-
-	obtenerValoresArchivoConfiguracion();
+	crearLogger();
+	obteneryValidarValoresArchivoConfiguracion();
 	imprimirArchivoConfiguracion();
-	pthread_mutex_init(&mutex_instancias,NULL);
-	pthread_mutex_init(&mutex_esis,NULL);
-	pthread_mutex_init(&mutex_claves,NULL);
-	esis=list_create();
-	claves_globales=list_create();
-	instancias=list_create();
+	//comprobarValoresBienSeteados();
+	pthread_mutex_init(&mutex_instancias, NULL);
+	pthread_mutex_init(&mutex_esis, NULL);
+	pthread_mutex_init(&mutex_claves, NULL);
+	esis = list_create();
+	claves_globales = list_create();
+	instancias = list_create();
 	ServidorConcurrente(IP, PUERTO, COORDINADOR, &listaHilos, &end, accion);
-
+	finalizarLogger();
 	return EXIT_SUCCESS;
 }
