55fa69b3403875e039fa7a119130062d3c63af81
Nadia Gutierrez <nadiagutierrez89@yahoo.com.ar>
2018-05-26 00:57:58 -0300

    subo el case MuerteEsi en el módulo Planificador

--- ESI/src/ESI.c
+++ ESI/src/ESI.c
@@ -15,49 +15,44 @@ pthread_t hiloCoordinador, hiloPlanificador, hiloParser;
 
 t_log* vg_logger;
 
-
 /* Creo el logger de ESI*/
 
 /*Creación de Logger*/
 void crearLogger() {
-	vg_logger = log_create("ESILog.log", "Coordinador", true,
-			LOG_LEVEL_INFO);
+	vg_logger = log_create("ESILog.log", "Coordinador", true, LOG_LEVEL_INFO);
 }
 
 void obtenerValoresArchivoConfiguracion() {
-	t_config* arch = config_create("/home/utnso/workspace/tp-2018-1c-Fail-system/ESI/esi.cfg");
-	IP_COORDINADOR = string_duplicate(config_get_string_value(arch, "IP_COORDINADOR"));
+	t_config* arch = config_create(
+			"/home/utnso/workspace/tp-2018-1c-Fail-system/ESI/esi.cfg");
+	IP_COORDINADOR = string_duplicate(
+			config_get_string_value(arch, "IP_COORDINADOR"));
 	PUERTO_COORDINADOR = config_get_int_value(arch, "PUERTO_COORDINADOR");
-	IP_PLANIFICADOR = string_duplicate(config_get_string_value(arch, "IP_PLANIFICADOR"));
+	IP_PLANIFICADOR = string_duplicate(
+			config_get_string_value(arch, "IP_PLANIFICADOR"));
 	PUERTO_PLANIFICADOR = config_get_int_value(arch, "PUERTO_PLANIFICADOR");
 	ID = string_duplicate(config_get_string_value(arch, "ID"));
 	config_destroy(arch);
 }
 
-void imprimirArchivoConfiguracion(){
-	printf(
-				"Configuración:\n"
-				"IP_COORDINADOR=%s\n"
-				"PUERTO_COORDINADOR=%d\n"
-				"IP_PLANIFICADOR=%s\n"
-				"PUERTO_PLANIFICADOR=%d\n"
-				"ID=%s\n",
-				IP_COORDINADOR,
-				PUERTO_COORDINADOR,
-				IP_PLANIFICADOR,
-				PUERTO_PLANIFICADOR,
-				ID
-				);
+void imprimirArchivoConfiguracion() {
+	printf("Configuración:\n"
+			"IP_COORDINADOR=%s\n"
+			"PUERTO_COORDINADOR=%d\n"
+			"IP_PLANIFICADOR=%s\n"
+			"PUERTO_PLANIFICADOR=%d\n"
+			"ID=%s\n", IP_COORDINADOR, PUERTO_COORDINADOR, IP_PLANIFICADOR,
+			PUERTO_PLANIFICADOR, ID);
 	fflush(stdout);
 }
 
-void enviarHandshakeESI(int socketFD,char emisor[13]){
-	Paquete* paquete = malloc(TAMANIOHEADER+strlen(ID)+1);
+void enviarHandshakeESI(int socketFD, char emisor[13]) {
+	Paquete* paquete = malloc(TAMANIOHEADER + strlen(ID) + 1);
 	Header header;
 	header.tipoMensaje = ESHANDSHAKE;
-	header.tamPayload = strlen(ID)+1;
-	paquete->Payload=malloc(strlen(ID)+1);
-	strcpy(paquete->Payload,ID);
+	header.tamPayload = strlen(ID) + 1;
+	paquete->Payload = malloc(strlen(ID) + 1);
+	strcpy(paquete->Payload, ID);
 	strcpy(header.emisor, emisor);
 	paquete->header = header;
 	EnviarPaquete(socketFD, paquete);
@@ -65,110 +60,124 @@ void enviarHandshakeESI(int socketFD,char emisor[13]){
 	free(paquete);
 }
 
-void escucharCoordinador(int socketFD,char emisor[13]){
+void escucharCoordinador(int socketFD, char emisor[13]) {
 	Paquete paquete;
-		void* datos;
-		while (RecibirPaqueteCliente(socketCoordinador, ESI, &paquete)>0){
-			datos=paquete.Payload;
-			switch(paquete.header.tipoMensaje)
-			{
-				//CASES
-			}
-
-			if (paquete.Payload != NULL){
-				free(paquete.Payload);
-			}
+	void* datos;
+	while (RecibirPaqueteCliente(socketCoordinador, ESI, &paquete) > 0) {
+		datos = paquete.Payload;
+		switch (paquete.header.tipoMensaje) {
+		//CASES
 		}
+
+		if (paquete.Payload != NULL) {
+			free(paquete.Payload);
+		}
+	}
 }
 
-void escucharPlanificador(int socketFD,char emisor[13]){
+void escucharPlanificador(int socketFD, char emisor[13]) {
 	Paquete paquete;
-		void* datos;
-		while (RecibirPaqueteCliente(socketPlanificador, ESI, &paquete)>0){
-			datos=paquete.Payload;
-			switch(paquete.header.tipoMensaje)
-			{
-				case SIGUIENTELINEA:
-				{
-					pthread_mutex_unlock(&binario_linea);
-				}
-				break;
-
-				case ABORTAR:
-				{
-					muerteEsi();
-				}
-				break;
-			}
-
-			if (paquete.Payload != NULL){
-				free(paquete.Payload);
-			}
+	void* datos;
+	while (RecibirPaqueteCliente(socketPlanificador, ESI, &paquete) > 0) {
+		datos = paquete.Payload;
+		switch (paquete.header.tipoMensaje) {
+		case SIGUIENTELINEA: {
+			pthread_mutex_unlock(&binario_linea);
 		}
-}
+			break;
 
-void parsear(){
-	FILE * fp;
-		char * line = NULL;
-		size_t len = 0;
-		ssize_t read;
-		void* datos;
-
-		fp = fopen(programaAEjecutar, "r");
-		if (fp == NULL){
-			perror("Error al abrir el archivo: ");
+		case ABORTAR: {
 			muerteEsi();
 		}
-
-		while ((read = getline(&line, &len, fp)) != -1) {
-			pthread_mutex_lock(&binario_linea);
-			t_esi_operacion parsed = parse(line);
-			if(parsed.valido){
-				switch(parsed.keyword){
-					case GET:
-						datos = malloc(strlen(parsed.argumentos.GET.clave)+1);
-						strcpy(datos, parsed.argumentos.GET.clave);
-						EnviarDatosTipo(socketCoordinador, ESI, datos, strlen(parsed.argumentos.GET.clave)+1, GETCOORD);
-						printf("GET\tclave: <%s>\n", parsed.argumentos.GET.clave);
-						log_info(vg_logger, "ESI id: %s, se ejecutó operación GET, con clave: %s", ID, parsed.argumentos.GET.clave);
-						break;
-					case SET:
-						datos = malloc(strlen(parsed.argumentos.SET.clave)+strlen(parsed.argumentos.SET.valor)+2);
-						strcpy(datos, parsed.argumentos.SET.clave);
-						strcpy(datos + strlen(parsed.argumentos.SET.clave) + 1, parsed.argumentos.SET.valor);
-						EnviarDatosTipo(socketCoordinador, ESI, datos, strlen(parsed.argumentos.SET.clave)+strlen(parsed.argumentos.SET.valor)+2, SETCOORD);
-						printf("SET\tclave: <%s>\tvalor: <%s>\n", parsed.argumentos.SET.clave, parsed.argumentos.SET.valor);
-						log_info(vg_logger, "ESI id: %s, se ejecutó operación SET, con clave: %s y valor: %s", ID,parsed.argumentos.SET.clave,parsed.argumentos.SET.valor);
-						break;
-					case STORE:
-						datos = malloc(strlen(parsed.argumentos.STORE.clave)+1);
-						strcpy(datos, parsed.argumentos.STORE.clave);
-						EnviarDatosTipo(socketCoordinador, ESI, datos, strlen(parsed.argumentos.STORE.clave)+1, STORECOORD);
-						printf("STORE\tclave: <%s>\n", parsed.argumentos.STORE.clave);
-						log_info(vg_logger, "ESI id: %s, se ejecutó operación STORE, con clave: %s", ID, parsed.argumentos.STORE.clave);
-						break;
-					default:
-						fprintf(stderr, "No pude interpretar <%s>\n", line);
-						muerteEsi();
-				}
-
-				destruir_operacion(parsed);
-			} else {
-				fprintf(stderr, "La linea <%s> no es valida\n", line);
-				muerteEsi();
-			}
+			break;
 		}
 
-		fclose(fp);
-		if (line)
-			free(line);
+		if (paquete.Payload != NULL) {
+			free(paquete.Payload);
+		}
+	}
+}
+
+void parsear() {
+	FILE * fp;
+	char * line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	void* datos;
+
+	fp = fopen(programaAEjecutar, "r");
+	if (fp == NULL) {
+		perror("Error al abrir el archivo: ");
 		muerteEsi();
+	}
+
+	while ((read = getline(&line, &len, fp)) != -1) {
+		pthread_mutex_lock(&binario_linea);
+		t_esi_operacion parsed = parse(line);
+		if (parsed.valido) {
+			switch (parsed.keyword) {
+			case GET:
+				datos = malloc(strlen(parsed.argumentos.GET.clave) + 1);
+				strcpy(datos, parsed.argumentos.GET.clave);
+				EnviarDatosTipo(socketCoordinador, ESI, datos,
+						strlen(parsed.argumentos.GET.clave) + 1, GETCOORD);
+				printf("GET\tclave: <%s>\n", parsed.argumentos.GET.clave);
+				log_info(vg_logger,
+						"ESI id: %s, se ejecutó operación GET, con clave: %s",
+						ID, parsed.argumentos.GET.clave);
+				break;
+			case SET:
+				datos = malloc(
+						strlen(parsed.argumentos.SET.clave)
+								+ strlen(parsed.argumentos.SET.valor) + 2);
+				strcpy(datos, parsed.argumentos.SET.clave);
+				strcpy(datos + strlen(parsed.argumentos.SET.clave) + 1,
+						parsed.argumentos.SET.valor);
+				EnviarDatosTipo(socketCoordinador, ESI, datos,
+						strlen(parsed.argumentos.SET.clave)
+								+ strlen(parsed.argumentos.SET.valor) + 2,
+						SETCOORD);
+				printf("SET\tclave: <%s>\tvalor: <%s>\n",
+						parsed.argumentos.SET.clave,
+						parsed.argumentos.SET.valor);
+				log_info(vg_logger,
+						"ESI id: %s, se ejecutó operación SET, con clave: %s y valor: %s",
+						ID, parsed.argumentos.SET.clave,
+						parsed.argumentos.SET.valor);
+				break;
+			case STORE:
+				datos = malloc(strlen(parsed.argumentos.STORE.clave) + 1);
+				strcpy(datos, parsed.argumentos.STORE.clave);
+				EnviarDatosTipo(socketCoordinador, ESI, datos,
+						strlen(parsed.argumentos.STORE.clave) + 1, STORECOORD);
+				printf("STORE\tclave: <%s>\n", parsed.argumentos.STORE.clave);
+				log_info(vg_logger,
+						"ESI id: %s, se ejecutó operación STORE, con clave: %s",
+						ID, parsed.argumentos.STORE.clave);
+				break;
+			default:
+				fprintf(stderr, "No pude interpretar <%s>\n", line);
+				muerteEsi();
+			}
+
+			destruir_operacion(parsed);
+		} else {
+			fprintf(stderr, "La linea <%s> no es valida\n", line);
+			muerteEsi();
+		}
+	}
+
+	fclose(fp);
+	if (line)
+		free(line);
+	muerteEsi();
 
 }
 
-void muerteEsi(){
+void muerteEsi() {
 	close(socketCoordinador);
-	EnviarDatosTipo(socketPlanificador, ESI, NULL, 0, MUERTEESI);
+
+	EnviarDatosTipo(socketPlanificador, ESI, ID, strlen(ID) + 1, MUERTEESI);
 	close(socketPlanificador);
 	terminar = true;
 }
@@ -180,12 +189,16 @@ int main(int argc, char* argv[]) {
 	printf("El programa a ejecutar es %s\n", programaAEjecutar);
 	fflush(stdout);
 	pthread_mutex_init(&binario_linea, NULL);
-	socketPlanificador = ConectarAServidorESI(PUERTO_PLANIFICADOR, IP_PLANIFICADOR, PLANIFICADOR, ESI, RecibirHandshake, enviarHandshakeESI);
-	socketCoordinador = ConectarAServidorESI(PUERTO_COORDINADOR, IP_COORDINADOR, COORDINADOR, ESI, RecibirHandshake, enviarHandshakeESI);
+	socketPlanificador = ConectarAServidorESI(PUERTO_PLANIFICADOR,
+			IP_PLANIFICADOR, PLANIFICADOR, ESI, RecibirHandshake,
+			enviarHandshakeESI);
+	socketCoordinador = ConectarAServidorESI(PUERTO_COORDINADOR, IP_COORDINADOR,
+	COORDINADOR, ESI, RecibirHandshake, enviarHandshakeESI);
 	pthread_create(&hiloCoordinador, NULL, (void*) escucharCoordinador, NULL);
 	pthread_create(&hiloPlanificador, NULL, (void*) escucharPlanificador, NULL);
 	pthread_create(&hiloParser, NULL, (void*) parsear, NULL);
-	while(!terminar);
+	while (!terminar)
+		;
 	pthread_join(hiloCoordinador, NULL);
 	pthread_join(hiloPlanificador, NULL);
 	pthread_join(hiloParser, NULL);
--- Planificador/src/Planificador.c
+++ Planificador/src/Planificador.c
@@ -4,7 +4,6 @@ char *IP, *ALGORITMO_PLANIFICACION, *IP_COORDINADOR;
 int PUERTO, ESTIMACION_INICIAL, PUERTO_COORDINADOR;
 char **CLAVES_BLOQUEADAS;
 
-
 int socketCoordinador;
 
 t_list* listaHilos;
@@ -15,8 +14,7 @@ bool end;
 t_list *clavesBloqueadas;
 t_list *LISTOS, *EJECUCION, *BLOQUEADOS, *TERMINADOS;
 
-
-void inicializar(){
+void inicializar() {
 	clavesBloqueadas = list_create();
 	LISTOS = list_create();
 	EJECUCION = list_create();
@@ -25,131 +23,129 @@ void inicializar(){
 }
 
 void obtenerValoresArchivoConfiguracion() {
-	t_config* arch = config_create("/home/utnso/workspace/tp-2018-1c-Fail-system/Planificador/planificador.cfg");
+	t_config* arch =
+			config_create(
+					"/home/utnso/workspace/tp-2018-1c-Fail-system/Planificador/planificador.cfg");
 	IP = string_duplicate(config_get_string_value(arch, "IP"));
 	PUERTO = config_get_int_value(arch, "PUERTO");
-	ALGORITMO_PLANIFICACION = string_duplicate(config_get_string_value(arch, "ALGORITMO_PLANIFICACION"));
+	ALGORITMO_PLANIFICACION = string_duplicate(
+			config_get_string_value(arch, "ALGORITMO_PLANIFICACION"));
 	ESTIMACION_INICIAL = config_get_int_value(arch, "ESTIMACION_INICIAL");
-	IP_COORDINADOR = string_duplicate(config_get_string_value(arch, "IP_COORDINADOR"));
+	IP_COORDINADOR = string_duplicate(
+			config_get_string_value(arch, "IP_COORDINADOR"));
 	PUERTO_COORDINADOR = config_get_int_value(arch, "PUERTO_COORDINADOR");
 	CLAVES_BLOQUEADAS = config_get_array_value(arch, "CLAVES_BLOQUEADAS");
 	config_destroy(arch);
 }
 
-void imprimirArchivoConfiguracion(){
-	printf(
-				"Configuración:\n"
-				"IP=%s\n"
-				"PUERTO=%d\n"
-				"ALGORITMO_PLANIFICACION=%s\n"
-				"ESTIMACION_INICIAL=%d\n"
-				"IP_COORDINADOR=%s\n"
-				"PUERTO_COORDINADOR=%d\n"
-				"CLAVES_BLOQUEADAS=\n",
-				IP,
-				PUERTO,
-				ALGORITMO_PLANIFICACION,
-				ESTIMACION_INICIAL,
-				IP_COORDINADOR,
-				PUERTO_COORDINADOR
-				);
+void imprimirArchivoConfiguracion() {
+	printf("Configuración:\n"
+			"IP=%s\n"
+			"PUERTO=%d\n"
+			"ALGORITMO_PLANIFICACION=%s\n"
+			"ESTIMACION_INICIAL=%d\n"
+			"IP_COORDINADOR=%s\n"
+			"PUERTO_COORDINADOR=%d\n"
+			"CLAVES_BLOQUEADAS=\n", IP, PUERTO, ALGORITMO_PLANIFICACION,
+			ESTIMACION_INICIAL, IP_COORDINADOR, PUERTO_COORDINADOR);
 
-	string_iterate_lines(CLAVES_BLOQUEADAS, LAMBDA(void _(char* item1)
-		{
-			clavexEsi* cxe = malloc(sizeof(clavexEsi));
-			cxe->idEsi = malloc(strlen("SYSTEM")+1);
-			cxe->clave = malloc(strlen(item1)+1);
-			cxe->borrar = false;
-			strcpy(cxe->idEsi, "SYSTEM");
-			strcpy(cxe->clave, item1);
-		    list_add(clavesBloqueadas, cxe);
-		    printf("\t\t%s\n",item1);
-		}));
+	string_iterate_lines(CLAVES_BLOQUEADAS,
+			LAMBDA(
+					void _(char* item1) { clavexEsi* cxe = malloc(sizeof(clavexEsi)); cxe->idEsi = malloc(strlen("SYSTEM")+1); cxe->clave = malloc(strlen(item1)+1); cxe->borrar = false; strcpy(cxe->idEsi, "SYSTEM"); strcpy(cxe->clave, item1); list_add(clavesBloqueadas, cxe); printf("\t\t%s\n",item1); }));
 	fflush(stdout);
 }
 
-void escuchaCoordinador(){
+void escuchaCoordinador() {
 	Paquete paquete;
 	void* datos;
-	while (RecibirPaqueteCliente(socketCoordinador, PLANIFICADOR, &paquete)>0){
-		datos=paquete.Payload;
-		switch(paquete.header.tipoMensaje)
-		{
-			case GETPLANI:
-			{
-				//Se fija si la clave que recibio está en la lista de claves bloqueadas
-				if(list_any_satisfy(clavesBloqueadas, LAMBDA(bool _(clavexEsi* item1){ return !strcmp(item1->clave, paquete.Payload);})))
-				{
-					//Si está, bloquea al proceso ESI
-					procesoEsi* esiABloquear = (procesoEsi*) list_remove_by_condition(EJECUCION, LAMBDA(bool _(procesoEsi* item1){ return !strcmp(item1->id, paquete.Payload + strlen(paquete.Payload)+1);}));
-					esiBloqueado* esiBloqueado = malloc(sizeof(esiBloqueado));
-					esiBloqueado->esi = esiABloquear;
-					esiBloqueado->clave = malloc(strlen(paquete.Payload)+1);
-					strcpy(esiBloqueado->clave, paquete.Payload);
-					list_add(BLOQUEADOS, esiBloqueado);
-				}
-				else
-				{
-					//Sino, agrega la clave a claves bloqueadas
-					clavexEsi* cxe = malloc(sizeof(clavexEsi));
-					cxe->idEsi = malloc(strlen(paquete.Payload + strlen(paquete.Payload) + 1) + 1);
-					cxe->clave = malloc(strlen(paquete.Payload) + 1);
-					cxe->borrar = false;
-					strcpy(cxe->idEsi, paquete.Payload + strlen(paquete.Payload) + 1);
-					strcpy(cxe->clave, paquete.Payload);
-					list_add(clavesBloqueadas, cxe);
-					procesoEsi* esiAEstarReady = (procesoEsi*) list_remove_by_condition(EJECUCION, LAMBDA(bool _(procesoEsi* item1){ return !strcmp(item1->id, paquete.Payload + strlen(paquete.Payload)+1);}));
-					list_add(LISTOS, esiAEstarReady);
-					//if (!strcmp(ALGORITMO_PLANIFICACION,"SJF/CD"))
-					//	planificar();
-				}
-
+	while (RecibirPaqueteCliente(socketCoordinador, PLANIFICADOR, &paquete) > 0) {
+		datos = paquete.Payload;
+		switch (paquete.header.tipoMensaje) {
+		case GETPLANI: {
+			//Se fija si la clave que recibio está en la lista de claves bloqueadas
+			if (list_any_satisfy(clavesBloqueadas,
+					LAMBDA(
+							bool _(clavexEsi* item1){ return !strcmp(item1->clave, paquete.Payload);}))) {
+				//Si está, bloquea al proceso ESI
+				procesoEsi* esiABloquear =
+						(procesoEsi*) list_remove_by_condition(EJECUCION,
+								LAMBDA(
+										bool _(procesoEsi* item1){ return !strcmp(item1->id, paquete.Payload + strlen(paquete.Payload)+1);}));
+				esiBloqueado* esiBloqueado = malloc(sizeof(esiBloqueado));
+				esiBloqueado->esi = esiABloquear;
+				esiBloqueado->clave = malloc(strlen(paquete.Payload) + 1);
+				strcpy(esiBloqueado->clave, paquete.Payload);
+				list_add(BLOQUEADOS, esiBloqueado);
+			} else {
+				//Sino, agrega la clave a claves bloqueadas
+				clavexEsi* cxe = malloc(sizeof(clavexEsi));
+				cxe->idEsi = malloc(
+						strlen(paquete.Payload + strlen(paquete.Payload) + 1)
+								+ 1);
+				cxe->clave = malloc(strlen(paquete.Payload) + 1);
+				cxe->borrar = false;
+				strcpy(cxe->idEsi,
+						paquete.Payload + strlen(paquete.Payload) + 1);
+				strcpy(cxe->clave, paquete.Payload);
+				list_add(clavesBloqueadas, cxe);
+				procesoEsi* esiAEstarReady =
+						(procesoEsi*) list_remove_by_condition(EJECUCION,
+								LAMBDA(
+										bool _(procesoEsi* item1){ return !strcmp(item1->id, paquete.Payload + strlen(paquete.Payload)+1);}));
+				list_add(LISTOS, esiAEstarReady);
+				//if (!strcmp(ALGORITMO_PLANIFICACION,"SJF/CD"))
+				//	planificar();
 			}
+
+		}
 			break;
 			/* PARA EL CASO DEL STORE
-			case MENSAJEQUEMEMANDEJULIPARASTORE:
-						{
-							//Se fija si la clave que recibio está en la lista de claves bloqueadas
-							if(list_any_satisfy(clavesBloqueadas, LAMBDA(bool _(clavexEsi* item1){ return !strcmp(item1->clave, paquete.Payload);})))
-							{
-								//Si está, bloquea al proceso ESI
-								procesoEsi* esiABloquear = (procesoEsi*) list_remove_by_condition(EJECUCION, LAMBDA(bool _(procesoEsi* item1){ return !strcmp(item1->id, paquete.Payload + strlen(paquete.Payload)+1);}));
-								esiBloqueado* esiBloqueado = malloc(sizeof(esiBloqueado));
-								esiBloqueado->esi = esiABloquear;
-								esiBloqueado->clave = malloc(strlen(paquete.Payload)+1);
-								strcpy(esiBloqueado->clave, paquete.Payload);
-								list_add(BLOQUEADOS, esiBloqueado);
-							}
-							else
-							{
-								//Sino, agrega la clave a claves bloqueadas
-								clavexEsi* cxe = malloc(sizeof(clavexEsi));
-								cxe->idEsi = malloc(strlen(paquete.Payload + strlen(paquete.Payload) + 1) + 1);
-								cxe->clave = malloc(strlen(paquete.Payload) + 1);
-								cxe->borrar = false;
-								strcpy(cxe->idEsi, paquete.Payload + strlen(paquete.Payload) + 1);
-								strcpy(cxe->clave, paquete.Payload);
-								list_add(clavesBloqueadas, cxe);
-								procesoEsi* esiAEstarReady = (procesoEsi*) list_remove_by_condition(EJECUCION, LAMBDA(bool _(procesoEsi* item1){ return !strcmp(item1->id, paquete.Payload + strlen(paquete.Payload)+1);}));
-								list_add(LISTOS, esiAEstarReady);
-								//if (!strcmp(ALGORITMO_PLANIFICACION,"SJF/CD"))
-								//	planificar();
-							}
+			 case MENSAJEQUEMEMANDEJULIPARASTORE:
+			 {
+			 //Se fija si la clave que recibio está en la lista de claves bloqueadas
+			 if(list_any_satisfy(clavesBloqueadas, LAMBDA(bool _(clavexEsi* item1){ return !strcmp(item1->clave, paquete.Payload);})))
+			 {
+			 //Si está, bloquea al proceso ESI
+			 procesoEsi* esiABloquear = (procesoEsi*) list_remove_by_condition(EJECUCION, LAMBDA(bool _(procesoEsi* item1){ return !strcmp(item1->id, paquete.Payload + strlen(paquete.Payload)+1);}));
+			 esiBloqueado* esiBloqueado = malloc(sizeof(esiBloqueado));
+			 esiBloqueado->esi = esiABloquear;
+			 esiBloqueado->clave = malloc(strlen(paquete.Payload)+1);
+			 strcpy(esiBloqueado->clave, paquete.Payload);
+			 list_add(BLOQUEADOS, esiBloqueado);
+			 }
+			 else
+			 {
+			 //Sino, agrega la clave a claves bloqueadas
+			 clavexEsi* cxe = malloc(sizeof(clavexEsi));
+			 cxe->idEsi = malloc(strlen(paquete.Payload + strlen(paquete.Payload) + 1) + 1);
+			 cxe->clave = malloc(strlen(paquete.Payload) + 1);
+			 cxe->borrar = false;
+			 strcpy(cxe->idEsi, paquete.Payload + strlen(paquete.Payload) + 1);
+			 strcpy(cxe->clave, paquete.Payload);
+			 list_add(clavesBloqueadas, cxe);
+			 procesoEsi* esiAEstarReady = (procesoEsi*) list_remove_by_condition(EJECUCION, LAMBDA(bool _(procesoEsi* item1){ return !strcmp(item1->id, paquete.Payload + strlen(paquete.Payload)+1);}));
+			 list_add(LISTOS, esiAEstarReady);
+			 //if (!strcmp(ALGORITMO_PLANIFICACION,"SJF/CD"))
+			 //	planificar();
+			 }
 
-						}
-						break;
-			*/
+			 }
+			 break;
+			 */
 
-			case ABORTAR:
-			{
-				procesoEsi* esiAAbortar = (procesoEsi*) list_remove_by_condition(EJECUCION, LAMBDA(bool _(procesoEsi* item1){ return !strcmp(item1->id, datos);}));
-				EnviarDatosTipo(esiAAbortar->socket, PLANIFICADOR, NULL, 0, ABORTAR);
-				list_add(TERMINADOS, esiAAbortar);
-			}
+		case ABORTAR: {
+			procesoEsi* esiAAbortar =
+					(procesoEsi*) list_remove_by_condition(EJECUCION,
+							LAMBDA(
+									bool _(procesoEsi* item1){ return !strcmp(item1->id, datos);}));
+			EnviarDatosTipo(esiAAbortar->socket, PLANIFICADOR, NULL, 0,
+					ABORTAR);
+			list_add(TERMINADOS, esiAAbortar);
+		}
 			break;
 		}
 
-		if (paquete.Payload != NULL){
+		if (paquete.Payload != NULL) {
 			free(paquete.Payload);
 		}
 	}
@@ -158,85 +154,137 @@ void escuchaCoordinador(){
 void accion(void* socket) {
 	int socketFD = *(int*) socket;
 	Paquete paquete;
-	while (RecibirPaqueteServidorPlanificador(socketFD, PLANIFICADOR, &paquete) > 0) {
+	while (RecibirPaqueteServidorPlanificador(socketFD, PLANIFICADOR, &paquete)
+			> 0) {
 		if (!strcmp(paquete.header.emisor, ESI)) {
-			switch(paquete.header.tipoMensaje)
-			{
-				case ESHANDSHAKE:
-					printf("El proceso ESI es %s\n", (char*)paquete.Payload);
-					fflush(stdout);
-					procesoEsi* nuevoEsi =  malloc(sizeof(procesoEsi));
-					nuevoEsi->id = malloc(strlen(paquete.Payload) + 1);
-					nuevoEsi->socket = socketFD;
-					strcpy(nuevoEsi->id, paquete.Payload);
-					list_add(LISTOS, nuevoEsi);
-					if (!strcmp(ALGORITMO_PLANIFICACION,"SJF/CD") || list_size(LISTOS)==1)
-						planificar();
-					break;
+			switch (paquete.header.tipoMensaje) {
+			case ESHANDSHAKE:
+				printf("El proceso ESI es %s\n", (char*) paquete.Payload);
+				fflush(stdout);
+				procesoEsi* nuevoEsi = malloc(sizeof(procesoEsi));
+				nuevoEsi->id = malloc(strlen(paquete.Payload) + 1);
+				nuevoEsi->socket = socketFD;
+				strcpy(nuevoEsi->id, paquete.Payload);
+				list_add(LISTOS, nuevoEsi);
+				if (!strcmp(ALGORITMO_PLANIFICACION, "SJF/CD")
+						|| list_size(LISTOS) == 1)
+					planificar();
+				break;
 
-				case MUERTEESI:
-					//liberarrecursos
-					break;
+			case MUERTEESI: {
+				char* idEsiFinalizado = (char*) paquete.Payload;
+				printf("El proceso ESI finalizado es %s\n", idEsiFinalizado);
+				fflush(stdout);
+				//libero claves bloqueadas por ese ESI
+				while (list_any_satisfy(clavesBloqueadas,
+						LAMBDA(
+								bool _(clavexEsi* item1) {return !strcmp(item1->idEsi,idEsiFinalizado);}))) {
+					clavexEsi* clavexEsiABorrar =
+							list_remove_by_condition(clavesBloqueadas,
+									LAMBDA(
+											bool _(clavexEsi* item1) {return !strcmp(item1->idEsi,idEsiFinalizado);}));
+					esiBloqueado* esiADesbloquear =
+							list_remove_by_condition(BLOQUEADOS,
+									LAMBDA(
+											bool _(esiBloqueado* esiBloqueado ){ return !strcmp(esiBloqueado->clave, clavexEsiABorrar->clave);}));
+					printf(
+							"Se desbloqueó el primer proceso ESI %s en la cola del recurso %s.\n",
+							esiADesbloquear->esi->id, esiADesbloquear->clave);
+					list_add(LISTOS, esiADesbloquear->esi);
+					free(esiADesbloquear->clave);
+					free(clavexEsiABorrar->clave);
+					free(clavexEsiABorrar->idEsi);
+					free(clavexEsiABorrar);
+				}//Busco en que cola esta y paso el ESI a la cola de TERMINADOS
+				if (list_any_satisfy(BLOQUEADOS,
+						LAMBDA(
+								bool _(esiBloqueado* esiBloqueado ) {return !strcmp(esiBloqueado->esi->id,idEsiFinalizado);}))) {
+
+					procesoEsi* esiTerminado =
+							list_remove_by_condition(BLOQUEADOS,
+									LAMBDA(
+											bool _(esiBloqueado* esiBloqueado ) {return !strcmp(esiBloqueado->esi->id,idEsiFinalizado);}));
+					list_add(TERMINADOS, esiTerminado);
+				} else if (list_any_satisfy(LISTOS,
+						LAMBDA(
+								bool _(procesoEsi* esi ) {return !strcmp(esi->id,idEsiFinalizado);}))) {
+
+					procesoEsi* esiTerminado =
+							list_remove_by_condition(LISTOS,
+									LAMBDA(
+											bool _(procesoEsi* esi ) {return !strcmp(esi->id,idEsiFinalizado);}));
+					list_add(TERMINADOS, esiTerminado);
+				} else if (list_any_satisfy(EJECUCION,
+						LAMBDA(
+								bool _(procesoEsi* esi ) {return !strcmp(esi->id,idEsiFinalizado);}))) {
+
+					procesoEsi* esiTerminado =
+							list_remove_by_condition(EJECUCION,
+									LAMBDA(
+											bool _(procesoEsi* esi ) {return !strcmp(esi->id,idEsiFinalizado);}));
+					list_add(TERMINADOS, esiTerminado);
+
+				}
+				break;
+
 			}
-		}
-		else
+
+			}
+		} else {
 			perror("No es ningún proceso ESI.\n");
-		if (paquete.Payload != NULL)
-			free(paquete.Payload);
+			if (paquete.Payload != NULL)
+				free(paquete.Payload);
+		}
+		close(socketFD);
 	}
-	close(socketFD);
 }
 
-
-void planificar()
-{
-	if (!planificacion_detenida)
-	{
-		if (!list_is_empty(LISTOS))
-		{
-			if (!strcmp(ALGORITMO_PLANIFICACION, "FIFO"))
-			{
+void planificar() {
+	if (!planificacion_detenida) {
+		if (!list_is_empty(LISTOS)) {
+			if (!strcmp(ALGORITMO_PLANIFICACION, "FIFO")) {
 				procesoEsi* esiAEjecutar = (procesoEsi*) list_remove(LISTOS, 0);
 				list_add(EJECUCION, esiAEjecutar);
-				EnviarDatosTipo(esiAEjecutar->socket,PLANIFICADOR, NULL, 0, SIGUIENTELINEA);
-			}
-			else if (!strcmp(ALGORITMO_PLANIFICACION, "SJF/SD"))
-			{
+				EnviarDatosTipo(esiAEjecutar->socket,
+				PLANIFICADOR, NULL, 0, SIGUIENTELINEA);
+			} else if (!strcmp(ALGORITMO_PLANIFICACION, "SJF/SD")) {
 
-			}
-			else if (!strcmp(ALGORITMO_PLANIFICACION, "SJF/CD"))
-			{
+			} else if (!strcmp(ALGORITMO_PLANIFICACION, "SJF/CD")) {
 
-			}
-			else if (!strcmp(ALGORITMO_PLANIFICACION, "HRRN"))
-			{
+			} else if (!strcmp(ALGORITMO_PLANIFICACION, "HRRN")) {
 
 			}
 		}
 	}
 }
 
-void EnviarHandshakePlani(int socketFD,char emisor[13]){
+void EnviarHandshakePlani(int socketFD, char emisor[13]) {
 	void *datos = malloc(0);
-	int tamanio = 0, cant=0;
+	int tamanio = 0, cant = 0;
 
-	string_iterate_lines(CLAVES_BLOQUEADAS, LAMBDA(void _(char* item1)
-		{
-			datos = realloc(datos, tamanio + strlen(item1) + 1);
-			strcpy(datos+tamanio, item1);
-			tamanio += strlen(item1) + 1;
-			cant++;
-			free(item1);
-		}));
+	string_iterate_lines(CLAVES_BLOQUEADAS, LAMBDA(void _(char* item1) {
+		datos = realloc(datos, tamanio + strlen(item1) + 1)
+		;
+		strcpy(datos + tamanio, item1)
+		;
+		tamanio += strlen(item1) + 1
+		;
+		cant++
+		;
+		free(item1)
+		;
+	}
+	));
 
-	Paquete* paquete = malloc(TAMANIOHEADER+sizeof(int)+tamanio);
+	Paquete* paquete = malloc(
+	TAMANIOHEADER + sizeof(int) + tamanio);
 	Header header;
 	header.tipoMensaje = ESHANDSHAKE;
 
 	header.tamPayload = tamanio + sizeof(int);
-	paquete->Payload=malloc(sizeof(int) + tamanio);
+	paquete->Payload = malloc(sizeof(int) + tamanio);
 
-	((int*)paquete->Payload)[0] = cant;
+	((int*) paquete->Payload)[0] = cant;
 	memcpy(paquete->Payload + sizeof(int), datos, tamanio);
 	free(datos);
 
@@ -255,13 +303,16 @@ int main(void) {
 	obtenerValoresArchivoConfiguracion();
 	imprimirArchivoConfiguracion();
 	planificacion_detenida = false;
-	socketCoordinador = ConectarAServidorPlanificador(PUERTO_COORDINADOR, IP_COORDINADOR, COORDINADOR, PLANIFICADOR, RecibirHandshake, EnviarHandshakePlani);
+	socketCoordinador = ConectarAServidorPlanificador(PUERTO_COORDINADOR,
+			IP_COORDINADOR, COORDINADOR,
+			PLANIFICADOR, RecibirHandshake, EnviarHandshakePlani);
 	pthread_t hiloCoordinador;
-	pthread_create(&hiloCoordinador, NULL, (void*) escuchaCoordinador, NULL);
+	pthread_create(&hiloCoordinador, NULL, (void*) escuchaCoordinador,
+	NULL);
 	pthread_t hiloConsola;
 	pthread_create(&hiloConsola, NULL, (void*) consola, NULL);
 	pthread_t hiloPlanificador;
-	ServidorConcurrente(IP,PUERTO, PLANIFICADOR, &listaHilos, &end, accion);
+	ServidorConcurrente(IP, PUERTO, PLANIFICADOR, &listaHilos, &end, accion);
 	pthread_join(hiloConsola, NULL);
 	pthread_join(hiloPlanificador, NULL);
 	pthread_join(hiloCoordinador, NULL);
