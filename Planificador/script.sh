da7a3a14f05e62fb39a848d17463a82cf93fc00f
Nadia Gutierrez <nadiagutierrez89@yahoo.com.ar>
2018-06-09 01:07:03 -0300

    subi script2

--- Planificador/script.sh
+++ Planificador/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gcc -I/home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/Consola.d" -MT"src/Consola.o" -o "src/Consola.o" "../src/Consola.c"
+
 mkdir workspace
 cd workspace
 git clone https://github.com/sisoputnfrba/so-commons-library
@@ -14,4 +14,10 @@ cd parsi
 sudo make install 
 cd 	..
 cd tp-2018-1c-Fail-system
-git checkout Check_2
+git checkout Check_3
+
+gcc -I/home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/Consola.d" -MT"src/Consola.o" -o "src/Consola.o" "../src/Consola.c"
+gcc -I/home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/Planificador.d" -MT"src/Planificador.o" -o "src/Planificador.o" "../src/Planificador.c"
+gcc -I/home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/helper.d" -MT"src/helper.o" -o "src/helper.o" "/home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas/helper.c"
+gcc -I/home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/sockets.d" -MT"src/sockets.o" -o "src/sockets.o" "/home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas/sockets.c"
+gcc  -o "Planificador"  ./src/Consola.o ./src/Planificador.o ./src/helper.o ./src/sockets.o   -lcommons -lreadline -lpthread
