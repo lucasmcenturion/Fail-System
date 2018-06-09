a5f9328615fbe9a4c980691e23893a23ab62821a
Nadia Gutierrez <nadiagutierrez89@yahoo.com.ar>
2018-06-09 13:40:22 -0300

    fix script

--- Compilar.sh
+++ Compilar.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "compilo Coordinador"
-gcc -Wall  /home/utnso/workspace/tp-2018-1c-Fail-system/Coodinador/src/Coordinador.c  /home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas/sockets.c /home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas/helper.c -o Coordinador   -lcommons -lpthread
+gcc -Wall  /home/utnso/workspace/tp-2018-1c-Fail-system/Coordinador/src/Coordinador.c  /home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas/sockets.c /home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas/helper.c -o Coordinador   -lcommons -lpthread
 
 echo "compilo Planificador"
-gcc -Wall /home/utnso/workspace/tp-2018-1c-Fail-system/Planificador/src/ Planificador.c  /home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas/sockets.c /home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas/helper.c /home/utnso/workspace/tp-2018-1c-Fail-system/Planificador/src/Consola.c  -o Planificador   -lcommons -lpthread -lreadline
+gcc -Wall /home/utnso/workspace/tp-2018-1c-Fail-system/Planificador/src/Planificador.c  /home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas/sockets.c /home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas/helper.c /home/utnso/workspace/tp-2018-1c-Fail-system/Planificador/src/Consola.c  -o Planificador   -lcommons -lpthread -lreadline
 echo "compilo ESI"
 gcc -Wall  /home/utnso/workspace/tp-2018-1c-Fail-system/ESI/src/ESI.c  /home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas/sockets.c /home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas/helper.c -o ESI   -lcommons -lpthread -lparsi
 echo "compilo Instancia"
