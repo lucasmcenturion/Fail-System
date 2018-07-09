0de6bef16d574f4c3d7d8e81d69bcff4840f8380
Nadia <nadiagutierrez89@yahoo.com.ar>
2018-07-09 18:43:18 -0300

    Cambio en scripts

--- Compilar.sh
+++ Compilar.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "compilo Coordinador"
-gcc -Wall  /home/utnso/workspace/tp-2018-1c-Fail-system/Coordinador/src/Coordinador.c  /home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas/sockets.c /home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas/helper.c -o Coordinador   -lcommons -lpthread
+gcc   /home/utnso/workspace/tp-2018-1c-Fail-system/Coordinador/src/Coordinador.c  /home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas/sockets.c /home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas/helper.c -o Coordinador   -lcommons -lpthread
 
 echo "compilo Planificador"
-gcc -Wall /home/utnso/workspace/tp-2018-1c-Fail-system/Planificador/src/Planificador.c  /home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas/sockets.c /home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas/helper.c /home/utnso/workspace/tp-2018-1c-Fail-system/Planificador/src/Consola.c  -o Planificador   -lcommons -lpthread -lreadline
+gcc  /home/utnso/workspace/tp-2018-1c-Fail-system/Planificador/src/Planificador.c  /home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas/sockets.c /home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas/helper.c /home/utnso/workspace/tp-2018-1c-Fail-system/Planificador/src/Consola.c  -o Planificador   -lcommons -lpthread -lreadline
 echo "compilo ESI"
-gcc -Wall  /home/utnso/workspace/tp-2018-1c-Fail-system/ESI/src/ESI.c  /home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas/sockets.c /home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas/helper.c -o ESI   -lcommons -lpthread -lparsi
+gcc   /home/utnso/workspace/tp-2018-1c-Fail-system/ESI/src/ESI.c  /home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas/sockets.c /home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas/helper.c -o ESI   -lcommons -lpthread -lparsi
 echo "compilo Instancia"
-gcc -Wall   /home/utnso/workspace/tp-2018-1c-Fail-system/Instancia/src/Instancia.c  /home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas/sockets.c /home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas/helper.c -o Instancia   -lcommons -lpthread 
+gcc    /home/utnso/workspace/tp-2018-1c-Fail-system/Instancia/src/Instancia.c  /home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas/sockets.c /home/utnso/workspace/tp-2018-1c-Fail-system/Bibliotecas/helper.c -o Instancia   -lcommons -lpthread 
 echo "Finalizo compilación"
--- scriptInicial.sh
+++ scriptInicial.sh
@@ -14,5 +14,7 @@ sudo make install
 cd 	..
 git clone https://github.com/sisoputnfrba/tp-2018-1c-Fail-system
 cd tp-2018-1c-Fail-system
-git checkout Check_2
+git checkout Check_4
 
+sudo cp /workspace/tp-2018-1c-Fail-system/Compilar.sh /home/utnso/
+sudo cp /workspace/tp-2018-1c-Fail-system/scriptInicial.sh /home/utnso/
