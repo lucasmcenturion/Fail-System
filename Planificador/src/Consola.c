5e5c0f27db60b6fae2be53c19cd2649d0bb58aee
lucasmcenturion <lucasmcenturion@gmail.com>
2018-07-14 13:33:35 -0300

    Deadlock hecho y prueba de deadlock anda y detalla esis

--- Planificador/src/Consola.c
+++ Planificador/src/Consola.c
@@ -66,44 +66,49 @@ void Bloquear(char* clave, char* id){
 
 void Desbloquear(char* clave, bool flagPrint){
 	clavexEsi* clavexEsiABorrar = list_remove_by_condition(clavesBloqueadas, LAMBDA(bool _(clavexEsi* item1){ return !strcmp(item1->clave, clave);}));
-	esiBloqueado* esiDesbloqueado = list_remove_by_condition(BLOQUEADOS, LAMBDA(bool _(esiBloqueado* item1){ return !strcmp(item1->clave, clavexEsiABorrar->clave);}));
-	if (esiDesbloqueado != NULL)
-	{		if(flagPrint)
-			log_info(logger,
-			"Se desbloqueó el primer proceso ESI %s en la cola del recurso %s.\n",
-			esiDesbloqueado->esi->id, esiDesbloqueado->clave);
-		procesoEsi* esiAPonerReady = malloc(sizeof(procesoEsi));
-		esiAPonerReady->id = malloc(strlen(esiDesbloqueado->esi->id)+1);
-		strcpy(esiAPonerReady->id, esiDesbloqueado->esi->id);
-		esiAPonerReady->rafagasEstimadas = esiDesbloqueado->esi->rafagasEstimadas;
-		esiAPonerReady->rafagasRealesEjecutadas = esiDesbloqueado->esi->rafagasRealesEjecutadas;
-		esiAPonerReady->socket = esiDesbloqueado->esi->socket;
-		esiAPonerReady->tiempoDeLlegada =tiempoActual;
-		clavexEsi* clavexEsiAAgregar = malloc(sizeof(clavexEsi));
-		clavexEsiAAgregar->clave = malloc(strlen(clave)+1);
-		strcpy(clavexEsiAAgregar->clave, clave);
-		clavexEsiAAgregar->idEsi = malloc(strlen(esiDesbloqueado->esi->id)+1);
-		strcpy(clavexEsiAAgregar->idEsi, esiDesbloqueado->esi->id);
-		clavexEsiAAgregar->instancia = malloc(strlen(clavexEsiABorrar->instancia)+1);
-		strcpy(clavexEsiAAgregar->instancia, clavexEsiABorrar->instancia);
-		list_add(clavesBloqueadas, clavexEsiAAgregar);
-		list_add(LISTOS, esiAPonerReady);
-		flag = 1;
-//		if (!strcmp(ALGORITMO_PLANIFICACION, "SJF-CD") || list_size(LISTOS) == 1){
-//			list_iterate(LISTOS, (void*) CalcularEstimacion);
-//		}
-		free(esiDesbloqueado->clave);
-		free(esiDesbloqueado->esi->id);
-		free(esiDesbloqueado->esi);
-		free(esiDesbloqueado);
+	if (clavexEsiABorrar != NULL){
+		esiBloqueado* esiDesbloqueado = list_remove_by_condition(BLOQUEADOS, LAMBDA(bool _(esiBloqueado* item1){ return !strcmp(item1->clave, clavexEsiABorrar->clave);}));
+		if (esiDesbloqueado != NULL)
+		{		if(flagPrint)
+				log_info(logger,
+				"Se desbloqueó el primer proceso ESI %s en la cola del recurso %s.\n",
+				esiDesbloqueado->esi->id, esiDesbloqueado->clave);
+			procesoEsi* esiAPonerReady = malloc(sizeof(procesoEsi));
+			esiAPonerReady->id = malloc(strlen(esiDesbloqueado->esi->id)+1);
+			strcpy(esiAPonerReady->id, esiDesbloqueado->esi->id);
+			esiAPonerReady->rafagasEstimadas = esiDesbloqueado->esi->rafagasEstimadas;
+			esiAPonerReady->rafagasRealesEjecutadas = esiDesbloqueado->esi->rafagasRealesEjecutadas;
+			esiAPonerReady->socket = esiDesbloqueado->esi->socket;
+			esiAPonerReady->tiempoDeLlegada =tiempoActual;
+			clavexEsi* clavexEsiAAgregar = malloc(sizeof(clavexEsi));
+			clavexEsiAAgregar->clave = malloc(strlen(clave)+1);
+			strcpy(clavexEsiAAgregar->clave, clave);
+			clavexEsiAAgregar->idEsi = malloc(strlen(esiDesbloqueado->esi->id)+1);
+			strcpy(clavexEsiAAgregar->idEsi, esiDesbloqueado->esi->id);
+			clavexEsiAAgregar->instancia = malloc(strlen(clavexEsiABorrar->instancia)+1);
+			strcpy(clavexEsiAAgregar->instancia, clavexEsiABorrar->instancia);
+			list_add(clavesBloqueadas, clavexEsiAAgregar);
+			list_add(LISTOS, esiAPonerReady);
+			flag = 1;
+	//		if (!strcmp(ALGORITMO_PLANIFICACION, "SJF-CD") || list_size(LISTOS) == 1){
+	//			list_iterate(LISTOS, (void*) CalcularEstimacion);
+	//		}
+			free(esiDesbloqueado->clave);
+			free(esiDesbloqueado->esi->id);
+			free(esiDesbloqueado->esi);
+			free(esiDesbloqueado);
+		}
+		if(clavexEsiABorrar){
+			free(clavexEsiABorrar->idEsi);
+			free(clavexEsiABorrar->valor);
+			free(clavexEsiABorrar->instancia);
+			free(clavexEsiABorrar);
+		}
+		ChequearPlanificacionYSeguirEjecutando();
 	}
-	if(clavexEsiABorrar){
-		free(clavexEsiABorrar->idEsi);
-		free(clavexEsiABorrar->valor);
-		free(clavexEsiABorrar->instancia);
-		free(clavexEsiABorrar);
+	else{
+		log_info(logger,"La clave %s no se encontraba bloqueada.", clave);
 	}
-	ChequearPlanificacionYSeguirEjecutando();
 
 }
 
@@ -186,64 +191,92 @@ void Status(char* clave){
 }
 
 
-bool deadlockRecursivo(char* inicial, t_list* listaDL, esiBloqueado* esi){
-	char* idEsi = ((clavexEsi*)list_find(clavesBloqueadas, LAMBDA(bool _(clavexEsi* item1){ return !strcmp(item1->clave, esi->clave);})))->idEsi;
-	esiBloqueado* bloqueado = list_find(BLOQUEADOS, LAMBDA(bool _(esiBloqueado* item1){ return !strcmp(item1->esi->id, idEsi);}));
-	if (NULL != bloqueado)
+bool deadlockRecursivo(char* inicial, t_list* listaDL, esiBloqueado* esi, int* iteracion)
+{
+	if(6 > iteracion[0])
 	{
-		if (!strcmp(bloqueado->clave,inicial)){
-			//hay deadlock
-			t_list* deadlock = list_create();
-			list_add(deadlock,bloqueado->esi->id);
-			list_add(deadlock, esi->esi->id);
-			list_add(listaDL, deadlock);
-			return true;
-		}
-		else
+		clavexEsi* cxe = list_find(clavesBloqueadas, LAMBDA(bool _(clavexEsi* item1){ return !strcmp(item1->clave, esi->clave);}));
+		if(cxe != NULL)
 		{
-			//aplicar recursividad
-			deadlockRecursivo(inicial, listaDL, bloqueado->clave);
+			char* idEsi = cxe->idEsi;
+			esiBloqueado* bloqueado = list_find(BLOQUEADOS, LAMBDA(bool _(esiBloqueado* item1){ return !strcmp(item1->esi->id, idEsi);}));
+			if (NULL != bloqueado)
+			{
+				if (!strcmp(bloqueado->clave,inicial))
+				{
+					//hay deadlock
+					t_list* deadlock = list_create();
+					list_add(deadlock,bloqueado->esi->id);
+					list_add(deadlock, esi->esi->id);
+					list_add(listaDL, deadlock);
+					return true;
+				}
+				else
+				{
+					//aplicar recursividad
+					iteracion[0]++;
+					deadlockRecursivo(inicial, listaDL, bloqueado, iteracion);
+				}
+			}
 		}
 	}
 	return false;
 }
 
-void Deadlock(){
+
+void Deadlock()
+{
 	int i;
 	t_list* deadlocks = list_create();
 	for (i=0; i < list_size(BLOQUEADOS); i++)
 	{
 		esiBloqueado* bloqueado = list_get(BLOQUEADOS,i);
-		char* claveInicial = ((clavexEsi*)list_find(clavesBloqueadas, LAMBDA(bool _(clavexEsi* item1){ return !strcmp(item1->idEsi, bloqueado->esi->id);})))->clave;
-		deadlockRecursivo(claveInicial, deadlocks, bloqueado);
+		t_list* claves = list_filter(clavesBloqueadas, LAMBDA(bool _(clavexEsi* item1){ return !strcmp(item1->idEsi, bloqueado->esi->id);}));
+		int j;
+		for (j = 0; j < list_size(claves); j++)
+		{
+			char* claveInicial = ((clavexEsi*)list_get(claves, j))->clave;
+			int* iteracion = malloc(sizeof(int));
+			iteracion[0]=0;
+			deadlockRecursivo(claveInicial, deadlocks, bloqueado, iteracion);
+			free(iteracion);
+		}
 
 	}
-	printf(list_size(deadlocks));
-	/*
-	for (i=0; i < list_size(clavesBloqueadas); i++)
-	{
-		clavexEsi* cxe = list_get(clavesBloqueadas,i);
-		esiBloqueado* esi = list_find(BLOQUEADOS, LAMBDA(bool _(esiBloqueado* item1){ return !strcmp(item1->esi->id, cxe->idEsi);}));
-		if(NULL != esi)
-		{
-			t_list* esis_bloqueados = list_filter(BLOQUEADOS, LAMBDA(bool _(esiBloqueado* item1){ return !strcmp(item1->clave, cxe->clave);}));
-			if (esis_bloqueados != NULL)
-			{
-				int j;
-				for (j=0; j < list_size(esis_bloqueados); j++)
-				{
-					esiBloqueado* bloqueado = list_get(esis_bloqueados,j);
-					t_list* claves = list_filter(clavesBloqueadas, LAMBDA(bool _(clavexEsi* item1){ return !strcmp(item1->idEsi, bloqueado->esi->id);}));
-					void f (clavexEsi* c){
-						if(!strcmp(c->clave,esi->clave))
-							log_info("Los ESIs %s y %s se encuentran en deadlock por las claves %s y %s", esi->esi->id, bloqueado->esi->id, esi->clave, bloqueado->clave);
-					}
-					list_iterate(claves, f);
-				}
 
+	list_iterate(deadlocks,LAMBDA(void _(t_list* item1){
+		list_sort(item1, LAMBDA(bool _(char* i1, char*i2){
+				return i1 < i2;
+			}));
+	}));
+	for (i=0; i< list_size(deadlocks); i++)
+	{
+		t_list* deadlock = list_get(deadlocks, i);
+		list_remove_by_condition(deadlocks, LAMBDA(bool _(t_list* item1){
+			if (list_size(item1) == list_size(deadlock))
+			{
+				bool rv = true;
+				int k=0;
+				list_iterate(item1,LAMBDA(void _(char* idDelEsi){
+					if(rv)
+						if (strcmp(idDelEsi, list_get(deadlock,k)))
+							rv = false;
+					k++;
+				}));
+				return rv;
 			}
-		}
-	}*/
+			else
+				return false;
+		}));
+	}
+	for (i=0; i< list_size(deadlocks); i++)
+	{
+		t_list* deadlock = list_get(deadlocks, i);
+		log_info(logger, "Deadlock %d entre ESIs:\n", (i+1));
+		list_iterate(deadlock,LAMBDA(void _(char* id){
+			log_info(logger, "\t\t\t- %s", id);
+		}));
+	}
 }
 
 void consola() {
@@ -282,7 +315,6 @@ void consola() {
 			string_iterate_lines(array_input,free);
 		free(array_input);
 		}
-
 		else if (!strncmp(linea, "kill ", 5)) {
 			char **array_input = string_split(linea, " ");
 			Kill(array_input[1]);
