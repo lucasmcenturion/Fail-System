224b8d8283c8c8c52a8b4c1251dd76b1fba66b86
Centu <lucasmcenturion@gmail.com>
2018-07-14 17:27:13 -0300

    .

--- insertarEjecutables.sh
+++ insertarEjecutables.sh
@@ -5,6 +5,6 @@ sudo cp /home/utnso/workspace/tp-2018-1c-Fail-system/Plani.sh /home/utnso/
 cd
 ln -s workspace/tp-2018-1c-Fail-system/ESI/Debug/./ESI
 ln -s workspace/tp-2018-1c-Fail-system/Instancia/Debug/./Instancia 
-ln -s workspace/tp-2018-1c-Fail-system/Instancia/Instancia.cfg
+ln -s workspace/tp-2018-1c-Fail-system/Instancia/instancia.cfg
 ln -s workspace/tp-2018-1c-Fail-system/ESI/esi.cfg 
 echo Finalizo copia
