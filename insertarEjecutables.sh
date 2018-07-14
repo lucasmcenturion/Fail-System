7c118a723a22781711755ae638bb7de4c8f04bc0
Centu <lucasmcenturion@gmail.com>
2018-07-14 17:34:01 -0300

    .

--- insertarEjecutables.sh
+++ insertarEjecutables.sh
@@ -7,4 +7,6 @@ ln -s workspace/tp-2018-1c-Fail-system/ESI/Debug/./ESI
 ln -s workspace/tp-2018-1c-Fail-system/Instancia/Debug/./Instancia 
 ln -s workspace/tp-2018-1c-Fail-system/Instancia/instancia.cfg
 ln -s workspace/tp-2018-1c-Fail-system/ESI/esi.cfg 
+ln -s workspace/tp-2018-1c-Fail-system/Coordinador/coordinador.cfg
+ln -s workspace/tp-2018-1c-Fail-system/Planificador/planificador.cfg
 echo Finalizo copia
